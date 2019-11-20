a3a15ba0ffa55442fd6582b2dff38dde7c56b532
yangjianhui <yangjianhui@ienglish.cn>
2019-11-20 17:56:28 +0800

    导入stetho

---
+++ app/src/main/java/com/yang/demo/App.java
@@ -0,0 +1,22 @@
+package com.yang.demo;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by yang on 2019/11/20.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+        new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .build();
+    }
+}
